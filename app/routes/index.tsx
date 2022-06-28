ee340c8d5cba0f564562bd62e44a8ab28b435dd3
Nguyễn Ngọc Tuấn <tuannn3@onemount.com>
2022-06-28 11:23:28 +0700

    Update home page with Spline

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,25 @@
+import type { LinksFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+
+import Spline from "@splinetool/react-spline";
+
+import stylesUrl from "~/styles/index.css";
+import tailwindStyles from "~/tailwind.css";
+
+export const links: LinksFunction = () => {
+	return [
+		{
+			rel: "stylesheet",
+			href: stylesUrl,
+		},
+		{ rel: "stylesheet", href: tailwindStyles },
+	];
+};
+
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+	return (
+		<div className="container">
+			<Spline scene="https://prod.spline.design/nKzouKpPRJKrHvau/scene.splinecode" />
+		</div>
+	);
 }
